689f5b0d63421851b49c4ee25818b8b67a9753c2
mermmin <112727850+mermmin@users.noreply.github.com>
2022-09-21 21:09:29 -0400

    Update student.c

--- student.c
+++ student.c
@@ -2,10 +2,8 @@
 
 int squareOfSmallest(int array[], int length) {
   int square = 0;
-  int array[] = {4,2,5};
   //automatically set the min to first element
   int min = array[0];
-  int length = 3;
   //first find the min of the array 
   for(int x = 0; x < length; x++)
   {
